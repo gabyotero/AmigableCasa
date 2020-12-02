4cfc534a8f52c694beca54dcb9dc843b4fca89b9
gabyotero <74226068+gabyotero@users.noreply.github.com>
2020-12-02 01:05:47 -0600

    ConsejosTerminado

--- app/src/main/java/com/gaby/amigablecasa/Consejo_lavado.java
+++ app/src/main/java/com/gaby/amigablecasa/Consejo_lavado.java
@@ -3,12 +3,21 @@ package com.gaby.amigablecasa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Consejo_lavado extends AppCompatActivity {
-
+    ImageView img_regresarlavado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consejo_lavado);
+        img_regresarlavado=findViewById(R.id.img_regresarlavado);
+        img_regresarlavado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
--- app/src/main/java/com/gaby/amigablecasa/Consejo_lluvia.java
+++ app/src/main/java/com/gaby/amigablecasa/Consejo_lluvia.java
@@ -1,14 +1,42 @@
 package com.gaby.amigablecasa;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Consejo_lluvia extends AppCompatActivity {
+    ImageView img_recolectar,img_regresarlluvia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consejo_lluvia);
+        img_recolectar=findViewById(R.id.img_recolectar);
+        img_regresarlluvia=findViewById(R.id.img_regresarlluvia);
+        img_recolectar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(Consejo_lluvia.this);
+                builder.setMessage("Al iniciar la lluvia, reúne todos los recipientes en los que recolectarás el agua; espera entre 10 o 15 minutos de que haya iniciado la lluvia y colócalos en diferentes partes. Lo recomendable es que esperes este tiempo, ya que así evitarás que los contaminantes caigan y el agua esté sucia.");
+                builder.setTitle("Recolectando agua de lluvia");
+                //builder.setIcon(R.drawable.aguawc);
+                //builder.setView(R.drawable.basura);
+                AlertDialog dialog=builder.create();
+                dialog.show();
+
+            }
+        });
+        img_regresarlluvia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+
     }
 }
