f90d443819d1705a5d50545e99b6392161434b95
gabyotero <74226068+gabyotero@users.noreply.github.com>
2020-12-01 19:37:55 -0600

    ConsejosCocinaWC

--- app/src/main/java/com/gaby/amigablecasa/Consejo_cocina.java
+++ app/src/main/java/com/gaby/amigablecasa/Consejo_cocina.java
@@ -3,12 +3,21 @@ package com.gaby.amigablecasa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Consejo_cocina extends AppCompatActivity {
-
+    ImageView img_regresarcocina;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consejo_cocina);
+        img_regresarcocina=findViewById(R.id.img_regresarcocina);
+        img_regresarcocina.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
--- app/src/main/java/com/gaby/amigablecasa/Consejo_wc.java
+++ app/src/main/java/com/gaby/amigablecasa/Consejo_wc.java
@@ -3,12 +3,21 @@ package com.gaby.amigablecasa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Consejo_wc extends AppCompatActivity {
-
+    ImageView img_regresarwc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consejo_wc);
+        img_regresarwc=findViewById(R.id.img_regresarwc);
+        img_regresarwc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
