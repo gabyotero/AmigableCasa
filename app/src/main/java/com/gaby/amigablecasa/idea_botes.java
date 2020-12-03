ebb4548be79599fa8aeba48d469b64d3dcfa3aaa
ItzajanaOsuna <74266358+ItzajanaOsuna@users.noreply.github.com>
2020-12-02 19:30:42 -0600

    Carrusel

---
+++ app/src/main/java/com/gaby/amigablecasa/idea_botes.java
@@ -0,0 +1,39 @@
+package com.gaby.amigablecasa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+public class idea_botes extends AppCompatActivity {
+    ViewFlipper v_flipper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_idea_botes);
+
+        int images[]= {R.drawable.bote1, R.drawable.bote2, R.drawable.bote3, R.drawable.bote4, R.drawable.bote5,
+                R.drawable.bote6,R.drawable.bote7, R.drawable.bote8,R.drawable.bote9, R.drawable.bote10};
+
+        v_flipper = findViewById(R.id.carrusel);
+
+        //sentencia de validacion
+        for(int image:images){
+            flipperImages(image);
+        }
+    }
+    public void flipperImages(int image){
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+
+        v_flipper.addView(imageView);
+        v_flipper.setFlipInterval(2000);
+        v_flipper.setAutoStart(true);
+
+        //animacion
+        v_flipper.setInAnimation(this, android.R.anim.slide_in_left);
+       v_flipper.setOutAnimation(this, android.R.anim.slide_out_right);
+
+    }
+}
--- app/src/main/java/com/gaby/amigablecasa/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/gaby/amigablecasa/ui/slideshow/SlideshowFragment.java
@@ -1,9 +1,11 @@
 package com.gaby.amigablecasa.ui.slideshow;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,10 +14,12 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.gaby.amigablecasa.Consejo_cocina;
+import com.gaby.amigablecasa.idea_botes;
 import com.gaby.amigablecasa.R;
 
 public class SlideshowFragment extends Fragment {
-
+    Button btn_idea, btn_avance;
     private SlideshowViewModel slideshowViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -28,6 +32,14 @@ public class SlideshowFragment extends Fragment {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+            }
+        });
+        btn_idea=(Button)root.findViewById(R.id.btn_idea);
+        btn_idea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getContext(), idea_botes.class);
+                startActivity(i);
             }
         });
         return root;
